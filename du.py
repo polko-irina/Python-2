960ebfd3cfbfefbf27dade4fce9b8b65d68f2aac
Iryna Polko <polko-irina@seznam.cz>
2023-05-08 22:10:41 +0200

    1

--- du.py
+++ du.py
@@ -5,6 +5,6 @@ data = pandas.read_csv("1976-2020-president.csv")
 
 data["Rank"] = data.groupby(["year"])["candidatevotes"].rank(method="min", ascending=False)
 print(data.head(15))
- 
+
 # viteze = data.sort_values(["Rank"] == 1)
 # print(viteze)
